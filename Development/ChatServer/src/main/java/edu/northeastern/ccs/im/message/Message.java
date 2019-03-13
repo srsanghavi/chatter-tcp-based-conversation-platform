7860f322aa478c67238fd758910301869c8182a2
Himanshu Budhia <budhia.himanshu96@gmail.com>
2019-03-13 15:11:24 -0400

    @MSD208SP19-21 #comment Message class with attributes

---
+++ Development/ChatServer/src/main/java/edu/northeastern/ccs/im/message/Message.java
@@ -0,0 +1,15 @@
+package edu.northeastern.ccs.im.message;
+
+import java.sql.Timestamp;
+
+public class Message {
+
+  private String messageID;
+  private String messageText;
+  private String messageSender;
+
+  private Timestamp creationTS;
+
+  
+
+}
