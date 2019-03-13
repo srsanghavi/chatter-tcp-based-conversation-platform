30e25213bde24f0a8709b149b0ad05b4e3af80b6
Himanshu Budhia <budhia.himanshu96@gmail.com>
2019-03-13 15:32:28 -0400

    @MSD208SP19-21 #comment Message class with constructor

--- Development/ChatServer/src/main/java/edu/northeastern/ccs/im/message/Message.java
+++ Development/ChatServer/src/main/java/edu/northeastern/ccs/im/message/Message.java
@@ -1,14 +1,44 @@
 package edu.northeastern.ccs.im.message;
 
+import java.util.Date;
 import java.sql.Timestamp;
+import java.util.UUID;
 
+/**
+ * Message class, which will be the smallest module.
+ */
 public class Message {
 
-  private String messageID;
-  private String messageText;
-  private String messageSender;
+  private String messageID;       // ID of the message
+  private String messageText;     // Message text
+  private String messageSender;   // Name of the sender
 
-  private Timestamp creationTS;
+  private Timestamp creationTS;   // The TS of message creation
+
+  private String threadID;        // ID of thread to which message belongs
+
+  private String conversationID;  // ID of conversation to which message belongs
+
+
+  /**
+   * Constructor to create a message object.
+   * @param sender sender of the message
+   * @param messageText the body of the message
+   * @param threadID the thread to which message belongs
+   * @param conversationID the conversation to which the message belongs
+   */
+  public Message(String sender, String messageText, String threadID, String conversationID){
+
+    messageID = UUID.randomUUID().toString();
+    this.messageText = messageText;
+    this.messageSender = sender;
+
+    this.creationTS = new Timestamp((new Date()).getTime());
+
+    this.threadID = threadID;
+    this.conversationID = conversationID;
+
+  }
 
   
 
