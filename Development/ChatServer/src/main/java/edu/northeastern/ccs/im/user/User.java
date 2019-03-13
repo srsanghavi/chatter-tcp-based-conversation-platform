b70f885fe5ee5ea83174dd8d8ec6b0d31ba405e3
Ram Prakash <arivuchelvan.r@husky.neu.edu>
2019-03-13 11:06:30 -0400

    @MSD208SP19-19 #comment updated javadoc for User

--- Development/ChatServer/src/main/java/edu/northeastern/ccs/im/user/User.java
+++ Development/ChatServer/src/main/java/edu/northeastern/ccs/im/user/User.java
@@ -3,22 +3,31 @@ package edu.northeastern.ccs.im.user;
 import java.util.UUID;
 import java.util.Date;
 
+/**
+ * Abstract User that defines the basic methods all Users have
+ */
 public abstract class User {
-    private String id;
-    private long creationTS;
-    private long modifiedTS;
-    private boolean active;
+
+    private String id; // UUID
+    private long creationTS; // Creation Timestamp
+    private long modifiedTS; // Last Modified Timestamp
+    private boolean active; // Active status
     private String username;
     private String password;
     private String first_name;
     private String last_name;
     private String email;
     private String phone;
-    private boolean dnd;
-    private long last_activityTS;
-    private boolean searchable;
+    private boolean dnd; // Do Not Disturb Status
+    private long last_activityTS; // Last Activity Timestamp
+    private boolean searchable; // Searchable?
 
 
+    /**
+     * Private Constructor to create User
+     * @param uname username
+     * @param pass password
+     */
     private User(String uname, String pass){
         username = uname;
         password = pass;
@@ -33,18 +42,31 @@ public abstract class User {
         searchable = true;
     }
 
+    /**
+     * Creates a new User
+     * @return User
+     */
     public abstract User createUser();
 
     // Getters
 
+    /**
+     * @return isActive
+     */
     public boolean isActive(){
         return active;
     }
 
+    /**
+     * @return username
+     */
     public String getuserName(){
         return username;
     }
 
+    /**
+     * @return firstname
+     */
     public String getFirstName(){
         if (first_name != null)
             return first_name;
@@ -52,6 +74,9 @@ public abstract class User {
             return "";
     }
 
+    /**
+     * @return lastname
+     */
     public String getLastName(){
         if (last_name != null)
             return last_name;
@@ -59,6 +84,9 @@ public abstract class User {
             return "";
     }
 
+    /**
+     * @return email
+     */
     public String getEmail(){
         if (email != null)
             return email;
@@ -66,14 +94,23 @@ public abstract class User {
             return "";
     }
 
+    /**
+     * @return dnd
+     */
     public boolean getDND(){
         return dnd;
     }
 
+    /**
+     * @return last_activityTS
+     */
     public long getLastActivityTS(){
         return last_activityTS;
     }
 
+    /**
+     * @return searchable
+     */
     public boolean isSearchable(){
         return searchable;
     }
