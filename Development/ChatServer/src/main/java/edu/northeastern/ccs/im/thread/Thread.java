366af7791191b9f238bacee5d2485e95d338b7fc
Himanshu Budhia <budhia.himanshu96@gmail.com>
2019-03-13 16:12:25 -0400

    @MSD208SP19-22 #comment Thread class updated with constructor and initialization method

--- Development/ChatServer/src/main/java/edu/northeastern/ccs/im/thread/Thread.java
+++ Development/ChatServer/src/main/java/edu/northeastern/ccs/im/thread/Thread.java
@@ -19,5 +19,28 @@ public class Thread {
   private String conversationID;  // ID of conversation to which thread belongs
   private ArrayList<String> messageID;  // List of messages in the thread
 
-  
+
+  /**
+   * Constructor to initialize a thread.
+   */
+  public Thread(String messageID, String conversationID){
+    if(threadID == null){               // Check to initialize the thread
+      InitializeThread(conversationID);
+    }
+    this.messageID.add(messageID);      // Add the new message to the thread list of messages
+    this.modifiedTS = new Timestamp((new Date()).getTime());  // Update modified timestamp
+  }
+
+  /**
+   * Private method to initialize the thread.
+   */
+  private void InitializeThread(String conversationID){
+    this.threadID = UUID.randomUUID().toString();     // Give a new ID for thread
+    this.creationTS = new Timestamp((new Date()).getTime());  // Initialize the creation timestamp
+    this.modifiedTS = new Timestamp((new Date()).getTime());  // Initialize the modified timestamp
+    messageID = new ArrayList<>();        // Initialize the message list
+    active = true;                        // Initialize the active status of thread
+    this.conversationID = conversationID; // Update conversation ID of the thread
+  }
+
 }
