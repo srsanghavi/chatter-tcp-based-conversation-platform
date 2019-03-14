d0836d19f1d20a90ca551d9dda61c8c877eb19a0
Ram Prakash <arivuchelvan.r@husky.neu.edu>
2019-03-13 21:34:14 -0400

    @MSD208SP19-19 #comment more tests for User methods

--- Development/ChatServer/src/test/java/edu/northeastern/ccs/im/user/UserTest.java
+++ Development/ChatServer/src/test/java/edu/northeastern/ccs/im/user/UserTest.java
@@ -3,6 +3,7 @@ package edu.northeastern.ccs.im.user;
 import org.junit.jupiter.api.Test;
 
 import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertNotSame;j
 
 public class UserTest {
     User user = new User();
@@ -17,6 +18,25 @@ public class UserTest {
     public void getUsername(){
         String username = user.getuserName(8);
         assertEquals(username, "ram");
+    }
+
+    @Test
+    public void getEmail(){
+        String email = user.getEmail(8);
+        assertEquals(email, "ram@prattle.com");
+        assertNotSame(email, "ram@prakash.com");
+    }
+
+    @Test
+    public void getFirstName(){
+        String firstName = user.getFirstName(8);
+        assertEquals(firstName, "Ram");
+    }
+
+    @Test
+    public void getLastName(){
+        String firstName = user.getLastName(8);
+        assertEquals(firstName, "Prakash");
     }
 
 }
