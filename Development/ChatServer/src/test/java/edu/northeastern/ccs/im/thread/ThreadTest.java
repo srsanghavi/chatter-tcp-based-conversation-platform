afb860323f2b1ebd5f9fb758ecf1d575e1bb85f0
Himanshu Budhia <budhia.himanshu96@gmail.com>
2019-03-14 23:49:20 -0400

    @MSD208SP19-33 Adding coverage for Thread active status

--- Development/ChatServer/src/test/java/edu/northeastern/ccs/im/thread/ThreadTest.java
+++ Development/ChatServer/src/test/java/edu/northeastern/ccs/im/thread/ThreadTest.java
@@ -5,6 +5,8 @@ import edu.northeastern.ccs.im.NetworkConnection;
 import edu.northeastern.ccs.im.client.IMConnection;
 import edu.northeastern.ccs.im.conversation.Conversation;
 import edu.northeastern.ccs.im.server.ClientRunnable;
+
+import org.junit.Before;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -22,26 +24,36 @@ import static junit.framework.TestCase.assertTrue;
 
 public class ThreadTest {
 
+  Message m1;
+  Message m2;
+
+  @Before
+  public void setup(){
+    m1 = Message.makeBroadcastMessage("hsbudhia","srsanghavi::" +
+            "Message from Thread test.");
+    m2 = Message.makeBroadcastMessage("hsbudhia","srsanghavi::Message " +
+            "from Thread test.");
+  }
+
   @Test
   public void checkThreadCreation(){
-    Message mHimanshuToShashwat = Message.makeBroadcastMessage("hsbudhia","srsanghavi::Test " +
-            "thread creation.");
-    Message mShashwatToHimanshu = Message.makeBroadcastMessage("hsbudhia","srsanghavi::Test " +
-            "thread creation.");
     Conversation conversation = new Conversation();
-    Thread thread = new Thread(mHimanshuToShashwat.getMessageID(),conversation.getId());
+    Thread thread = new Thread(m1.getMessageID(),conversation.getId());
     System.out.println(thread.getThreadID());
   }
 
   @Test
   public void checkAddMessageToThread(){
-    Message m1 = Message.makeBroadcastMessage("hsbudhia","srsanghavi::" +
-            "Message from Thread test.");
-    Message m2 = Message.makeBroadcastMessage("hsbudhia","srsanghavi::Message " +
-            "from Thread test.");
     Conversation conversation = new Conversation();
     Thread thread = new Thread(m1.getMessageID(),conversation.getId());
     thread.addMessageToThread(m2.getMessageID());
   }
-  
+
+  @Test
+  public void checkThreadActive(){
+    Conversation conversation = new Conversation();
+    Thread thread = new Thread(m1.getMessageID(),conversation.getId());
+    assertTrue(thread.isActive());
+  }
+
 }
