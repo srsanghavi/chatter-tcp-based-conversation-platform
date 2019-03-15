6624e9489881e8c3062d42a7c8c5d9c4af99cd2b
Himanshu Budhia <budhia.himanshu96@gmail.com>
2019-03-14 23:45:22 -0400

    @MSD208SP19-33 Adding coverage for Thread class

--- Development/ChatServer/src/test/java/edu/northeastern/ccs/im/thread/ThreadTest.java
+++ Development/ChatServer/src/test/java/edu/northeastern/ccs/im/thread/ThreadTest.java
@@ -3,6 +3,7 @@ package edu.northeastern.ccs.im.thread;
 import edu.northeastern.ccs.im.Message;
 import edu.northeastern.ccs.im.NetworkConnection;
 import edu.northeastern.ccs.im.client.IMConnection;
+import edu.northeastern.ccs.im.conversation.Conversation;
 import edu.northeastern.ccs.im.server.ClientRunnable;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -20,4 +21,27 @@ import static junit.framework.TestCase.assertTrue;
 
 
 public class ThreadTest {
+
+  @Test
+  public void checkThreadCreation(){
+    Message mHimanshuToShashwat = Message.makeBroadcastMessage("hsbudhia","srsanghavi::Test " +
+            "thread creation.");
+    Message mShashwatToHimanshu = Message.makeBroadcastMessage("hsbudhia","srsanghavi::Test " +
+            "thread creation.");
+    Conversation conversation = new Conversation();
+    Thread thread = new Thread(mHimanshuToShashwat.getMessageID(),conversation.getId());
+    System.out.println(thread.getThreadID());
+  }
+
+  @Test
+  public void checkAddMessageToThread(){
+    Message m1 = Message.makeBroadcastMessage("hsbudhia","srsanghavi::" +
+            "Message from Thread test.");
+    Message m2 = Message.makeBroadcastMessage("hsbudhia","srsanghavi::Message " +
+            "from Thread test.");
+    Conversation conversation = new Conversation();
+    Thread thread = new Thread(m1.getMessageID(),conversation.getId());
+    thread.addMessageToThread(m2.getMessageID());
+  }
+  
 }
