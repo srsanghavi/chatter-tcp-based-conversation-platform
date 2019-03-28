d6c96cd943feec52280145c4cf00def580cb6ff0
shashwat <shashwat@mobile-sphere.com>
2019-03-28 19:16:40 -0400

    fix static method related errors

--- Development/ChatServer/src/test/java/edu/northeastern/ccs/im/RouteTest.java
+++ Development/ChatServer/src/test/java/edu/northeastern/ccs/im/RouteTest.java
@@ -1,43 +1,54 @@
 package edu.northeastern.ccs.im;
 
+import edu.northeastern.ccs.im.api.ApiMessageType;
 import edu.northeastern.ccs.im.api.Route;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class RouteTest {
-
+    ApiMessageType apiType;
+    @BeforeEach
+    void setUp(){
+         apiType = new ApiMessageType();
+    }
     @Test
     public void testGetConversations(){
-        System.out.println(Route.getResponseGet("getConversations/", "{user_id:1}"));
+        System.out.println(Route.getResponseGet("srsanghavi","getConversations/", "{user_id:1}"));
     }
 
     @Test
     public void testCreateUserDuplicateUser(){
-        System.out.println(Route.getResponsePost("registerUser/","{first_name:test;last_name:user;username:testUser;email:test@prattle.com;password:'12345678'}"));
+        System.out.println(Route.getResponsePost("srsanghavi","registerUser/","{first_name:test;last_name:user;username:testUser;email:test@prattle.com;password:'12345678'}"));
     }
 
     @Test
     public void testGetUserGroups(){
-        System.out.println(Route.getResponseGet("getGroups/","{user_id:1}"));
+        System.out.println(Route.getResponseGet("srsanghavi","getGroups/","{user_id:1}"));
     }
 
     @Test
     public void testBroadcastMessage(){
-        ChatLogger.info(Route.getResponsePost("broadcastMessage/", "{message:'hello world';sender_id:8}"));
+        ChatLogger.info(Route.getResponsePost("srsanghavi","broadcastMessage/", "{message:'hello world';sender_id:8}"));
     }
 
     @Test
     public void testGetMessagesinConversation(){
-        ChatLogger.info(Route.getResponseGet("getMessagesInConversation/", "{conversation_id:'205'}"));
+        ChatLogger.info(Route.getResponseGet("srsanghavi","getMessagesInConversation/", "{conversation_id:'205'}"));
     }
 
     @Test
     public void testGetMessagesinThread(){
-        ChatLogger.info(Route.getResponseGet("getMessagesInThread/", "{thread_id:'562'}"));
+        ChatLogger.info(Route.getResponseGet("srsanghavi","getMessagesInThread/", "{thread_id:'562'}"));
     }
 
     @Test
     public void testGetAllGroups(){
-        ChatLogger.info(Route.getResponseGet("getAllGroups/", "{user_id:'8'}"));
+        ChatLogger.info(Route.getResponseGet("srsanghavi","getAllGroups/", "{user_id:'8'}"));
+    }
+
+    @Test
+    public void testDeleteUser(){
+        ChatLogger.info(Route.getResponsePost("srsanghavi", "deleteUser/","{user_id:1}"));
     }
 
 }
