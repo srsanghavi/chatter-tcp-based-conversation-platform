c210b6a37dbba67fd60bfd4c7a67f25a6903690b
shashwat <shashwat@mobile-sphere.com>
2019-03-28 02:08:29 -0400

    @MSD208SP19-81 add factory method for models

---
+++ Development/ChatServer/src/main/java/edu/northeastern/ccs/im/database/ModelFactory.java
@@ -0,0 +1,36 @@
+package edu.northeastern.ccs.im.database;
+
+import edu.northeastern.ccs.im.group.Group;
+
+/**
+ * The type Model factory.
+ */
+public class ModelFactory {
+
+    /**
+     * Get user model user model.
+     *
+     * @return the user model
+     */
+    public static UserModel getUserModel(){
+        return new UserModel();
+    }
+
+    /**
+     * Get conversation model conversation model.
+     *
+     * @return the conversation model
+     */
+    public static ConversationModel getConversationModel(){
+        return new ConversationModel();
+    }
+
+    /**
+     * Get group model group model.
+     *
+     * @return the group model
+     */
+    public static GroupModel getGroupModel(){
+        return new GroupModel();
+    }
+}
