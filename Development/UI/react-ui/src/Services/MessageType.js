1d2eded5a04796f183a695702812905dc5c742e5
shashwat <shashwat@mobile-sphere.com>
2019-03-29 18:29:39 -0400

    fix UI bugs

--- Development/UI/react-ui/src/Services/MessageType.js
+++ Development/UI/react-ui/src/Services/MessageType.js
@@ -1,17 +1,29 @@
 export default class MessageType {
     makeLoginMsg(username,password){
+        if(text!="" || text==null){
+            text="";
+        }
         return 'HLO '+ username.length + ' ' + username + ' ' + password.length + ' ' + password;
     }
 
     makeTerminateMessage(username){
+        if(text!="" || text==null){
+            text="";
+        }
         return 'BYE ' + username.length + ' ' + username + ' ' + 0 + ' ' + '';
     }
 
     makeBroadcastMessage(username, text){
+        if(text!="" || text==null){
+            text="";
+        }
         return 'BCT ' + username.length + ' ' + username + ' ' + text.length + ' ' + text;
     }
 
     makeApiMessage(username, text){
+        if(text!="" || text==null){
+            text="";
+        }
         return 'API ' + username.length + ' ' + username + ' ' + text.length + ' ' + text;
     }
 }
