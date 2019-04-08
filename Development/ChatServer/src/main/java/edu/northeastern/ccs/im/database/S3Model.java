4d6e19ce5d291c1bbbcc1f729f84a89e04119e71
Ram Prakash <arivuchelvan.r@husky.neu.edu>
2019-04-08 11:12:54 -0400

    @MSD208SP19-94 #comment Updated region

--- Development/ChatServer/src/main/java/edu/northeastern/ccs/im/database/S3Model.java
+++ Development/ChatServer/src/main/java/edu/northeastern/ccs/im/database/S3Model.java
@@ -1,5 +1,7 @@
 package edu.northeastern.ccs.im.database;
 
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.Bucket;
@@ -9,6 +11,7 @@ public class S3Model {
 
     public String getBucket(){
         final AmazonS3 s3 = AmazonS3ClientBuilder.defaultClient();
+        s3.setRegion(Region.getRegion(Regions.US_EAST_1));
         List<Bucket> buckets = s3.listBuckets();
         return buckets.get(0).getName();
     }
