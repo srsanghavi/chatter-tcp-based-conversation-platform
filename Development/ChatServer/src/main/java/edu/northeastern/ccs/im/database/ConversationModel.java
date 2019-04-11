c309839b22501018a4829d1bbebb748ec8a60821
Ram Prakash <arivuchelvan.r@husky.neu.edu>
2019-04-11 18:29:28 -0400

    @MSD208SP19-95 #comment Changed tests for ConversationModel

--- Development/ChatServer/src/main/java/edu/northeastern/ccs/im/database/ConversationModel.java
+++ Development/ChatServer/src/main/java/edu/northeastern/ccs/im/database/ConversationModel.java
@@ -250,4 +250,15 @@ public class ConversationModel {
         return conn.getLastInsertedID();
     }
 
+    /**
+     * Delete conversation record permanently
+     * @param id conversation ID
+     * @return Result code
+     */
+    public int deleteConversationPermanently(int id){
+        String query = "DELETE FROM conversations WHERE id=?";
+        List<String> args = new ArrayList<>(Arrays.asList(Integer.toString(id)));
+        int r = conn.sqlcreate(query, args);
+        return r<=0?-1:r;
+    }
 }
